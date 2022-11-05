467f429c92e54d0a084d1d3e7b426ef7025238a9
unknown <mwm0022@126.com>
2022-11-05 21:26:48 +0800

    compress size>1mb or png or width>1920

--- compress.js
+++ compress.js
@@ -2,12 +2,13 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const rootPath = 'D:/BaiduNetdiskDownload/[3D韩国画师]DecalShooter大佬画师 极品丝袜'; //把父文件夹地址填充于此！
+const rootPath = 'D:/BaiduNetdiskDownload/[エロマズン (まー九郎)] 鬼滅の刃 系列'; //把父文件夹地址填充于此！
 
 async function compress() {
   let files = walk(rootPath);
   for (let index = 0; index < files.length; index++) {
     const file = files[index];
+    const stat = fs.statSync(file);
     const metadata = await sharp(file).metadata();
     const extname = path.extname(file);
     const filename = path.basename(file, extname);
@@ -19,6 +20,11 @@ async function compress() {
       if (metadata.format == 'png') {
         await sharp(file).toFile(dirname + '/' + filename + '_compress.jpg');
         fs.unlinkSync(file);
+      } else {
+        if ((stat.size / (1024 * 1000)) > 1) {
+          await sharp(file).jpeg({quality: 80}).toFile(dirname + '/' + filename + '_compress.jpg');
+          fs.unlinkSync(file);
+        }
       }
     }
   }
